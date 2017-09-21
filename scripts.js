a0a9791b24fc1e4eb97cb121029a100ab98044ec
Nilson Molina <MolinaNilson@gmail.com>
2017-09-20 22:21:43 -0400

    splash section started

--- scripts.js
+++ scripts.js
@@ -1,11 +1,10 @@
 window.onscroll = function() {
     var navbar = document.getElementById('navbar');
     var splash = document.getElementById('splash');
-    if ( window.pageYOffset > (splash.scrollHeight - navbar.clientHeight)) {
+    
+    if ( window.pageYOffset > (splash.clientHeight - navbar.clientHeight)) {
         navbar.classList.add("opaque");
-        console.log('greater than splash')
     } else {
         navbar.classList.remove("opaque");
-        console.log('less than splash')
     }
 }
